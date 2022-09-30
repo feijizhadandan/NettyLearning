164a6ed101456149301e0ce874515ad8b25cf1dc
飞机炸蛋 <215628266@qq.com>
2022-09-30 22:16:40 +0800

    update

---
+++ src/main/java/Netty/demo_division_work/HelloClient.java
@@ -0,0 +1,30 @@
+package Netty.demo_division_work;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nsc) throws Exception {
+                        nsc.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress(8888))
+                .sync()
+                .channel()
+                // 向服务器发送数据
+                .writeAndFlush("hello world");
+        
+    }
+}
