31e171218a0fc5895cc0c7d55b095ae58ab3c6cf
zhen <215628266@qq.com>
2022-10-10 21:35:31 +0800

    ByteBuf使用

---
+++ src/main/java/cn/bytebuf/TestByteBuf.java
@@ -0,0 +1,33 @@
+package cn.bytebuf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufUtil;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class TestByteBuf {
+    public static void main(String[] args) {
+
+
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(5);
+
+        // 1、写入整数（整数占4个字节，因此打印了四个元素）
+        buffer.writeInt(5);
+        System.out.println(buffer);
+        byte[] bytes = ByteBufUtil.getBytes(buffer);
+        for (byte aByte : bytes) {
+            System.out.println(aByte);
+        }
+
+        // 2、自动扩容
+        buffer.writeInt(8);
+        System.out.println(buffer);
+        bytes = ByteBufUtil.getBytes(buffer);
+        for (byte aByte : bytes) {
+            System.out.println(aByte);
+        }
+    }
+}
